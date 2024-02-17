39f494cba059512fdb03e293ec8d44b3e0b094a4
Metehan Uçan <76853222+metehanucan@users.noreply.github.com>
2024-02-17 11:42:01 +0100

    metehan change

--- ann/weaviate_ann.py
+++ ann/weaviate_ann.py
@@ -1,5 +1,4 @@
 import weaviate
-client = weaviate.Client("172.21.0.2:8080")  # Replace the URL with that of your Weaviate instance
 import utils
 import pandas as pd
 import numpy as np
@@ -7,19 +6,103 @@ import uuid
 import time
 import csv
 
+client = weaviate.Client("172.21.0.2:8080")  # Replace the URL with that of your Weaviate instance
+base_vectors = pd.DataFrame({'vector' : utils.read_fvecs('/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_base.fvecs').tolist()})
+query_vectors = pd.DataFrame({'vector' : utils.read_fvecs('/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_query.fvecs').tolist()})
+#truth = pd.DataFrame(utils.read_ivecs('/Users/metehan/Desktop/datasets/small/siftsmall_groundtruth.ivecs'))
+truth = utils.top_k_neighbors(query_vectors, base_vectors, k=100, function='euclidean', filtering=False)
+
+ef = [64]
+maxConnections = [8, 64]
+efConstruction = [64, 256]
+k_values = [1,10,100]
+
+""" client = weaviate.Client("172.21.0.2:8080")  # Replace the URL with that of your Weaviate instance
 base_vectors = pd.DataFrame({'vector': utils.read_fvecs("/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_base.fvecs").tolist()})
 query_vectors = pd.DataFrame({'vector': utils.read_fvecs("/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_query.fvecs").tolist()})
-
-
-truth = utils.top_k_neighbors(query_vectors, base_vectors, k=100, function='euclidean', filtering=False)
-k_number = [1, 10, 100]
+truth = 0 """
 
 def generate_uuid():
     return str(uuid.uuid4())
+
 base_vectors['uuid'] = base_vectors.apply(lambda row: generate_uuid(), axis=1)
 print('uuid generated')
 
-# truth contains the IDs of the k nearest neighbors that also satisfy the attribute filtering clause
+def main():
+    for ef_val in ef:
+        for m_val in maxConnections:
+            for efc_val in efConstruction:
+                to_name = 'Siftsmall_HSNW_index' + '_' + str(ef_val) + '_' + str(m_val) + '_' + str(efc_val)
+                create_HSNW_collections(ef_val, m_val, efc_val, to_name)
+                print('appended', to_name)
+    create_flat_index()
+
+def upload_data(class_name):
+    vectors = base_vectors["vector"].tolist()
+    uuids_in_dataframe = base_vectors["uuid"].tolist()
+    data_objs = [
+        {"title": f"Object {i+1}"} for i in range(len(base_vectors))
+    ]
+    uuids_in_dataframe = base_vectors["uuid"].tolist()
+    vectors = base_vectors["vector"].tolist()
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:
+        for i, data_obj in enumerate(data_objs):
+            batch.add_data_object(
+                data_obj,
+                class_name,
+                vector=vectors[i],
+                uuid= uuids_in_dataframe[i]
+            )
+
+def create_flat_index():
+    #with Flat index
+    class_name = "Siftsmall_flat_index" 
+    distance_metric = "l2-squared"
+    client.schema.delete_class(class_name)
+    # Class definition object. Weaviate's autoschema feature will infer properties when importing.
+    class_obj = {
+        "class": class_name,
+        "vectorizer": "none",
+        "vectorIndexType": "flat",
+        "vectorIndexConfig": {
+        "distance": distance_metric
+        }
+    }
+    # Add the class to the schema
+    client.schema.create_class(class_obj)
+    upload_data(class_name)
+    print(class_name, ' uploaded')
+    for k in k_values:
+        run_query(class_name, k, query_vectors, base_vectors)
+    client.schema.delete_class(class_name)
+
+def create_HSNW_collections(ef, maxConnections, efConstruction, to_name):
+    class_name = to_name
+    distance_metric = "l2-squared"
+    client.schema.delete_class(class_name)
+
+    # Class definition object. Weaviate's autoschema feature will infer properties when importing.
+    class_obj = {
+        "class": class_name,
+        "vectorizer": "none",
+        "vectorIndexType": "hnsw",
+        "vectorIndexConfig":{
+            "skip": False,
+            "pq": {"enabled": False,},
+            "maxConnections": maxConnections,
+            "efConstruction": efConstruction,
+            "ef": ef,
+            "distance": distance_metric}
+    }
+    # Add the class to the schema
+    client.schema.create_class(class_obj)
+    upload_data(class_name)
+
+    for k in k_values:
+        run_query(class_name, k, query_vectors, base_vectors)
+    client.schema.delete_class(class_name)
+
 def run_query(name, k, query_vec, base_vec):
 
     result = []
@@ -51,101 +134,27 @@ def run_query(name, k, query_vec, base_vec):
     for i in result_ids:
         result_indexes.append(base_vec[base_vec['uuid'].isin(i)].index)
 
+    metrics(end_time, start_time, name, result_indexes, result, k)
+
+def metrics(end_time, start_time, name, result_indexes, result, k):
     time_taken = end_time - start_time
     queries_count = len(result)
     throughput = queries_count / time_taken
     average_latency = time_taken / queries_count
-    print(f"Throughput: {throughput:.2f} queries per second")
-    print(f"Average Latency: {average_latency:.4f} seconds")
+    print(f"Throughput: of {name} {throughput:.2f} queries per second")
+    print(f"Average Latency: of {name} {average_latency:.4f} seconds")
+    print(f"k is {k}")
 
     true_positives = 0
     n_classified = 0
     for i,elem in enumerate(result_indexes):
-        true_positives_iter = len(np.intersect1d(truth[i], elem))
+        true_positives_iter = len(np.intersect1d(truth[i][:k], elem))
         true_positives += true_positives_iter
         n_classified += len(elem)
-    print(f'Average recall: {true_positives/n_classified}')
+    print(f'Average recall: of {name} {true_positives/n_classified}')
+    
     with open("results_ann_weviate.csv", mode='a') as file:
         writer = csv.writer(file)
         writer.writerow([name, k, (true_positives/n_classified), throughput, average_latency])
 
-def upload_data(class_name):
-    vectors = base_vectors["vector"].tolist()
-    uuids_in_dataframe = base_vectors["uuid"].tolist()
-    data_objs = [
-        {"title": f"Object {i+1}"} for i in range(len(base_vectors))
-    ]
-    uuids_in_dataframe = base_vectors["uuid"].tolist()
-    vectors = base_vectors["vector"].tolist()
-    client.batch.configure(batch_size=100)  # Configure batch
-    with client.batch as batch:
-        for i, data_obj in enumerate(data_objs):
-            batch.add_data_object(
-                data_obj,
-                class_name,
-                vector=vectors[i],
-                uuid= uuids_in_dataframe[i]
-            )
-#-------------------------------------------------------------------#
-#with Flat index
-class_name = "Siftsmall_flat_index" 
-distance_metric = "l2-squared"
-client.schema.delete_class(class_name)
-# Class definition object. Weaviate's autoschema feature will infer properties when importing.
-class_obj = {
-    "class": class_name,
-    "vectorizer": "none",
-    "vectorIndexType": "flat",
-    "vectorIndexConfig": {
-    "distance": distance_metric
-    }
-}
-# Add the class to the schema
-client.schema.create_class(class_obj)
-upload_data(class_name)
-#-------------------------------------------------------------------#
-def create_HSNW_collections(ef, maxConnections, efConstruction, to_name):
-    class_name = to_name
-    #with HSNW
-    distance_metric = "l2-squared"
-    client.schema.delete_class(class_name)
-    # Class definition object. Weaviate's autoschema feature will infer properties when importing.
-    class_obj = {
-        "class": class_name,
-        "vectorizer": "none",
-        "vectorIndexType": "hnsw",
-        "vectorIndexConfig":{
-            "skip": False,
-            "cleanupIntervalSeconds": 300,
-            "pq": {"enabled": False,},
-            "maxConnections": maxConnections,
-            "efConstruction": efConstruction,
-            "ef": ef,
-            "dynamicEfMin": 100,
-            "dynamicEfMax": 500,
-            "dynamicEfFactor": 8,
-            "vectorCacheMaxObjects": 2000000,
-            "flatSearchCutoff": 40000,
-            "distance": distance_metric},
-    }
-    # Add the class to the schema
-    client.schema.create_class(class_obj)
-    upload_data(class_name)
-    for k_ in k_number:
-        run_query(to_name, k_, query_vectors, base_vectors)
-    client.schema.delete_class(class_name)
-
-ef = [64, 128, 256, 512]
-maxConnections = [8, 16, 32, 64]
-efConstruction = [64, 128, 256, 512]
-
-for i in ef:
-    for j in maxConnections:
-        for ef_cons in efConstruction:
-            to_name = 'Siftsmall_HSNW_index' + '_' + str(i) + '_' + str(j) + '_' + str(ef_cons)
-            create_HSNW_collections(i,j,ef_cons, to_name)
-            print('appended', to_name)
-#-------------------------------------------------------------------#
-
-run_query("Siftsmall_flat_index", 100, query_vectors, base_vectors)
-
+main()
