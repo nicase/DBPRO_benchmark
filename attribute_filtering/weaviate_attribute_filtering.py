86041381ac7ad43c789e3273b13da2dc32bd0121
Metehan Uçan <76853222+metehanucan@users.noreply.github.com>
2024-02-17 13:15:58 +0100

    attribute filtering changes

--- attribute_filtering/weaviate_attribute_filtering.py
+++ attribute_filtering/weaviate_attribute_filtering.py
@@ -1,5 +1,4 @@
 import weaviate
-client = weaviate.Client("172.21.0.2:8080")  # Replace the URL with that of your Weaviate instance
 import utils
 import pandas as pd
 import numpy as np
@@ -7,27 +6,40 @@ import uuid
 import time
 import csv
 import random
+import pickle
+import os
+from dotenv import load_dotenv
+from os import getenv
 
-base_vectors = pd.DataFrame({'vector': utils.read_fvecs("/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_base.fvecs").tolist()})
-query_vectors = pd.DataFrame({'vector': utils.read_fvecs("/data-ssd/dimitrios/DBPRO_benchmark/data/sift/sift_query.fvecs").tolist()})
+load_dotenv()
+url = f"http://{os.getenv('WEAVIATE_URL')}:{os.getenv('WEAVIATE_PORT')}"
+print(f"Connecting to {url}...")
+client = weaviate.Client(url)  # Replace the URL with that of your Weaviate instance
 
-base_vectors_with_attributes = pd.DataFrame({'vector': base_vectors.tolist()})
-query_vectors_with_attributes = pd.DataFrame({'vector': query_vectors.tolist()})
+ef = [64, 128, 256, 512]
+maxConnections =  [8, 16, 32, 64]
+efConstruction = [64, 128, 256, 512]
+k_values = [1,10,100]
 
-num_rows = len(query_vectors_with_attributes)
-query_vectors_with_attributes['attr1'] = [random.choice([True, False]) for _ in range(num_rows)]
-query_vectors_with_attributes['attr2'] = [random.choice([True, False]) for _ in range(num_rows)]
-query_vectors_with_attributes['attr3'] = [random.choice([True, False]) for _ in range(num_rows)]
+def main():
+    for ef_val in ef:
+        for m_val in maxConnections:
+            for efc_val in efConstruction:
+                to_name = 'Siftsmall_HSNW_index' + '_' + str(ef_val) + '_' + str(m_val) + '_' + str(efc_val)
+                create_HSNW_collections(ef_val, m_val, efc_val, to_name)
+                print('appended', to_name)
+    create_flat_index()
 
-def generate_uuid():
-    return str(uuid.uuid4())
+def data_pickle():
+    with open(os.getenv('PICKLE_QUERY_VECTORS_PATH'), 'rb') as f:
+        base_vectors_with_attributes = pickle.load(f)
+    with open(os.getenv('PICKLE_BASE_VECTORS_PATH'), 'rb') as f:
+        query_vectors_with_attributes = pickle.load(f)
+    with open(os.getenv('PICKLE_GROUND_TRUTH_PATH'), 'rb') as f:
+        truth = pickle.load(f)
+    return base_vectors_with_attributes, query_vectors_with_attributes, truth
 
-num_rows = len(base_vectors_with_attributes)
-base_vectors_with_attributes['attr1'] = [random.choice([True, False]) for _ in range(num_rows)]
-base_vectors_with_attributes['attr2'] = [random.choice([True, False]) for _ in range(num_rows)]
-base_vectors_with_attributes['attr3'] = [random.choice([True, False]) for _ in range(num_rows)]
-base_vectors_with_attributes['uuid'] = base_vectors_with_attributes.apply(lambda row: generate_uuid(), axis=1)
-
+base_vectors_with_attributes, query_vectors_with_attributes, truth = data_pickle()
 
 def upload_data(class_name):
     data_objs = [
@@ -36,14 +48,6 @@ def upload_data(class_name):
     ]
     vectors = base_vectors_with_attributes["vector"].tolist()
     uuids_in_dataframe = base_vectors_with_attributes["uuid"].tolist()
-
-    data_objs = [
-        {"attr1": attr1, "attr2": attr2, "attr3": attr3}
-        for attr1, attr2, attr3 in zip(base_vectors_with_attributes["attr1"].tolist(), base_vectors_with_attributes["attr2"].tolist(), base_vectors_with_attributes["attr3"].tolist())
-    ]
-    uuids_in_dataframe = base_vectors_with_attributes["uuid"].tolist()
-
-    vectors = base_vectors_with_attributes["vector"].tolist()
     client.batch.configure(batch_size=100)  # Configure batch
     with client.batch as batch:
         for i, data_obj in enumerate(data_objs):
@@ -54,36 +58,38 @@ def upload_data(class_name):
                 uuid= uuids_in_dataframe[i]
             )
 
-#with Flat index
-class_name = "Siftsmall_flat_index" 
-distance_metric = "l2-squared"
-client.schema.delete_class(class_name)
-
-# Class definition object. Weaviate's autoschema feature will infer properties when importing.
-class_obj = {
-    "class": class_name,
-    "vectorizer": "none",
-    "vectorIndexType": "flat",
-    "vectorIndexConfig": {
-    "distance": distance_metric
-    },
-     "properties": [{
-        "name": "attr1",
-        "dataType": ["boolean"]},{
-        "name": "attr2",
-        "dataType": ["boolean"]},{
-        "name": "attr3",
-        "dataType": ["boolean"]}, 
-        ]
-}
-
-# Add the class to the schema
-client.schema.create_class(class_obj)
-upload_data(class_name)
+def create_flat_index():
+    #with Flat index
+    class_name = "Siftsmall_flat_index" 
+    distance_metric = "l2-squared"
+    client.schema.delete_class(class_name)
+    # Class definition object. Weaviate's autoschema feature will infer properties when importing.
+    class_obj = {
+        "class": class_name,
+        "vectorizer": "none",
+        "vectorIndexType": "flat",
+        "vectorIndexConfig": {
+        "distance": distance_metric
+        },
+        "properties": [{
+            "name": "attr1",
+            "dataType": ["boolean"]},{
+            "name": "attr2",
+            "dataType": ["boolean"]},{
+            "name": "attr3",
+            "dataType": ["boolean"]}, 
+            ]
+    }
+    # Add the class to the schema
+    client.schema.create_class(class_obj)
+    upload_data(class_name)
+    print(class_name, ' uploaded')
+    for k in k_values:
+        run_query(class_name, k, query_vectors_with_attributes, base_vectors_with_attributes)
+    client.schema.delete_class(class_name)
 
 def create_HSNW_collections(ef, maxConnections, efConstruction, to_name):
     class_name = to_name
-    #with HSNW
     distance_metric = "l2-squared"
     client.schema.delete_class(class_name)
 
@@ -94,16 +100,10 @@ def create_HSNW_collections(ef, maxConnections, efConstruction, to_name):
         "vectorIndexType": "hnsw",
         "vectorIndexConfig":{
             "skip": False,
-            "cleanupIntervalSeconds": 300,
             "pq": {"enabled": False,},
             "maxConnections": maxConnections,
             "efConstruction": efConstruction,
             "ef": ef,
-            "dynamicEfMin": 100,
-            "dynamicEfMax": 500,
-            "dynamicEfFactor": 8,
-            "vectorCacheMaxObjects": 2000000,
-            "flatSearchCutoff": 40000,
             "distance": distance_metric},
         "properties": [{
                 "name": "attr1",
@@ -114,35 +114,18 @@ def create_HSNW_collections(ef, maxConnections, efConstruction, to_name):
                 "dataType": ["boolean"]}, 
                 ]
     }
-
     # Add the class to the schema
     client.schema.create_class(class_obj)
-    upload_data(class_name)
+    upload_data(class_name, )
 
-
-ef = [64, 128, 256, 512]
-maxConnections = [8, 16, 32, 64]
-efConstruction = [64, 128, 256, 512]
-names = []
-
-for i in ef:
-    for j in maxConnections:
-        for k in efConstruction:
-            to_name = 'Siftsmall_HSNW_index' + '_' + str(i) + '_' + str(j) + '_' + str(k)
-            names.append(to_name)
-            print('appending', to_name)
-            create_HSNW_collections(i,j,k, to_name)
-            print('appended', to_name)
-
-# truth contains the IDs of the k nearest neighbors that also satisfy the attribute filtering clause
-base = base_vectors_with_attributes.drop("uuid", axis=1)
-truth = utils.top_k_neighbors(query_vectors_with_attributes, base, k=100, function='euclidean', filtering=True)
+    for k in k_values:
+         run_query(class_name, k, query_vectors_with_attributes, base_vectors_with_attributes)
+    client.schema.delete_class(class_name)
 
 def run_query(name, k, query_vec, base_vec):
-
     result = []
     start_time = time.time()
-    for _,elem in query_vec.iterrows():
+    for _, elem in query_vec.iterrows():
         
         vec = elem["vector"]
         attr1 = elem["attr1"]
@@ -181,7 +164,7 @@ def run_query(name, k, query_vec, base_vec):
         )
         result.append(response)
     end_time = time.time()
-   
+
     result_ids = []
     for i in range(len(query_vec["vector"])):
         result_ids.append([])
@@ -191,7 +174,11 @@ def run_query(name, k, query_vec, base_vec):
     result_indexes = []
     for i in result_ids:
         result_indexes.append(base_vec[base_vec['uuid'].isin(i)].index)
+        print(base_vec[base_vec['uuid'].isin(i)].index)
 
+    metrics(end_time, start_time, name, result_indexes, result, k)
+
+def metrics(end_time, start_time, name, result_indexes, result, k):
     time_taken = end_time - start_time
     queries_count = len(result)
     throughput = queries_count / time_taken
@@ -200,11 +187,12 @@ def run_query(name, k, query_vec, base_vec):
     print(f"Average Latency: of {name} {average_latency:.4f} seconds")
     print(f"k is {k}")
 
-
     true_positives = 0
     n_classified = 0
     for i,elem in enumerate(result_indexes):
-        true_positives_iter = len(np.intersect1d(truth[i], elem))
+        print(truth[i])
+
+        true_positives_iter = len(np.intersect1d(truth[i][:k], elem))
         true_positives += true_positives_iter
         n_classified += len(elem)
     print(f'Average recall: of {name} {true_positives/n_classified}')
@@ -212,26 +200,3 @@ def run_query(name, k, query_vec, base_vec):
     with open("results_attribute_weviate.csv", mode='a') as file:
         writer = csv.writer(file)
         writer.writerow([name, k, (true_positives/n_classified), throughput, average_latency])
-    # sum = 0
-    # for i,j in enumerate(result_indexes):
-    #     intersection = np.intersect1d(truth[i], j)
-    #     sum = sum + len(intersection)
-    # print(f'The average accuracy is {sum/len(result_indexes)}')
-
-k_number = [1, 10, 100]
-for i in names:
-    for j in range(3):
-        run_query(i, k_number[j], query_vectors_with_attributes, base_vectors_with_attributes)
-
-run_query("Siftsmall_flat_index" , 100, query_vectors_with_attributes, base_vectors_with_attributes)
-# Specify the file name
-
-csv_file_name = "results_attribute_weviate.csv"
-# Define the column names
-columns = ["name", "k", "average_recall", "throughput", "latency"]
-# Create an empty CSV file with header
-with open(csv_file_name, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    # Write the header to the CSV file
-    writer.writerow(columns)
-print(f"Empty CSV file '{csv_file_name}' with columns {columns} has been created.")
