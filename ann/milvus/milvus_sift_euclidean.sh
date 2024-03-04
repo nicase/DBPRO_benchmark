3baedd7b2dff0cbb252d1be14dddf834947a974a
ikkkayalar <62773436+ikkkayalar@users.noreply.github.com>
2024-03-04 14:12:59 +0100

    Add files via upload
    milvus ann implemented

---
+++ ann/milvus/milvus_sift_euclidean.sh
@@ -0,0 +1,16 @@
+docker-compose -f docker-compose-mlv.yml up -d
+
+container_id="milvus-standalone"
+
+python3 milvus_ann_sift_euclidean.py &
+
+python_pid=$!
+
+while ps -p $python_pid > /dev/null; do
+
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    docker stats "$container_id" --no-stream --format "{{.CPUPerc}},{{.MemUsage}},$timestamp"
+    sleep 5
+
+done
+docker-compose -f docker-compose-mlv.yml down
